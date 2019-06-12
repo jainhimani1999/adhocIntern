69213424a68e079b66ac40c8dfdc86561cd11c9b
jainhimani1999 <41498644+jainhimani1999@users.noreply.github.com>
2019-06-12 08:59:46 +0530

    Update problem-6.py

--- problem-6.py
+++ problem-6.py
@@ -3,7 +3,7 @@ file = open("file.txt", "r")
 #print(file.read())
 
 #for cat -E command
-'''for line in file:
+for line in file:
     print(line.strip() + "$")
 
 #for cat -n command
@@ -12,7 +12,7 @@ for line in file:
     print(str(count)+line.strip())
     count+=1
 #for output redirection cat file > file1
-'''
+
 location = input('Enter the file to be overwritten')
 file2 = open(location,"w")
 file2.write(file.read())
